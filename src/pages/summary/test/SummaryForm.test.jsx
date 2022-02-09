6ab953368812337e39a7d6b377348850311c0ca1
Felix Maldonado <felix_maldonado@ncci.com>
2022-02-09 10:05:16 -0500

    Added code for popups

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable no-restricted-globals */
 import SummaryForm from '../SummaryForm.jsx'
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, /*fireEvent*/ waitForElementToBeRemoved } from '@testing-library/react';  // fireEvent is commented out cause we replaced with userEvent
+                                                                         // userEvent is a better choice to use over fireEvent   \
+                                                                       
+import userEvent from '@testing-library/user-event';
 
 test('Initial Conditions', () => {
     render(<SummaryForm />);
@@ -18,11 +21,31 @@ test('Checkbox disables button on first click and enables on second click', () =
     const button = screen.getByRole('button', {name: /confirm order/i});
     const checkbox = screen.getByRole('checkbox', {name: /terms and conditions/i});
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
     expect (checkbox).toBeChecked();
     expect (button).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
     expect (checkbox).not.toBeChecked();
     expect (button).toBeDisabled();
+});
+
+test('popover responds to hover', async () => {
+    render(<SummaryForm />);
+    
+    //popover starts out hidden
+    const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+    expect (nullPopover).not.toBeInTheDocument();
+
+    //popover appears upon mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i)
+    userEvent.hover(termsAndConditions);
+
+    const popover = screen.getByText(/no ice cream will actually be delivered/i);
+    expect(popover).toBeInTheDocument();
+
+    //popover disappears when we mouse out
+    userEvent.unhover(termsAndConditions);
+    await waitForElementToBeRemoved(() => screen.queryByText(/no ice cream will actualy be delivered/i));
+
 });
